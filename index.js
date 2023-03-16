22a417befed5f60b7406431e820559607eb2d26d
SMLukwiya <sundaymorganl@gmail.com>
2023-03-16 14:57:03 +0300

    update vercel.json

--- index.js
+++ index.js
@@ -6,4 +6,12 @@ app.get('/', (req, res) => {
     res.send("Home")
 })
 
+app.get('/test', (req, res) => {
+    res.send("Test route working")
+})
+
+app.post('/test', (req, res) => {
+    res.send({message: "Received test data", body: req.body})
+})
+
 app.listen(3000, () => console.log("Listening on port", 3000))
